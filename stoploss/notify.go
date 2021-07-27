2decdee791561d961160add4e0b013c8ed62ef5e
Giancarlos Salas <giansalex@gmail.com>
2021-07-26 22:36:31 -0500

    Implement multiple notifiers

--- stoploss/notify.go
+++ stoploss/notify.go
@@ -1,43 +1,29 @@
 package stoploss
 
 import (
-	"fmt"
 	"log"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	bNotify "github.com/giansalex/binance-stoploss/notify"
 )
 
 // Notify notify stoploss
 type Notify struct {
-	tlgToken string
-	chatID   int64
+	inners []bNotify.SingleNotify
 }
 
 // NewNotify create Notify instance
-func NewNotify(telegramToken string, channelID int64) *Notify {
-	return &Notify{telegramToken, channelID}
+func NewNotify(notifiers []bNotify.SingleNotify) *Notify {
+	return &Notify{notifiers}
 }
 
 // Send send message
-func (notify *Notify) Send(message string) {
-	log.Println(message)
-
-	if notify.tlgToken == "" {
-		return
+func (notify *Notify) Send(message string) error {
+	for _, v := range notify.inners {
+		err := v.Send(message)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 
-	bot, err := tgbotapi.NewBotAPI(notify.tlgToken)
-	if err != nil {
-		fmt.Println("Cannot connect to Telegram:", err.Error())
-
-		return
-	}
-
-	msg := tgbotapi.NewMessage(notify.chatID, message)
-
-	_, err = bot.Send(msg)
-
-	if err != nil {
-		fmt.Println("Cannot send message to telegram:", err.Error())
-	}
+	return nil
 }
