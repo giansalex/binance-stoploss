39a49e37f9b9dffd38d1e7ee986e418187a58e4a
Giancarlos Salas <giansalex@gmail.com>
2021-07-27 11:06:34 -0500

    Clear html to log output

--- notify/log_notify.go
+++ notify/log_notify.go
@@ -1,13 +1,24 @@
 package notify
 
-import "log"
+import (
+	"log"
+	"regexp"
+)
+
+const regex = `<.*?>`
 
 type LogNotify struct {
 }
 
 // Send notify to log
 func (lnotify *LogNotify) Send(message string) error {
-	log.Println(message)
+	log.Println(lnotify.clearHtml(message))
 
 	return nil
+}
+
+// This method uses a regular expresion to remove HTML tags.
+func (lnotify *LogNotify) clearHtml(s string) string {
+	r := regexp.MustCompile(regex)
+	return r.ReplaceAllString(s, "")
 }
